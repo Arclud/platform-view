773890569abdf96fb8194f010a8a255c47f978a9
Rustam Bakytov <rustam.baktybek@pricer.kg>
2022-06-02 17:21:06 +0900

    platform-view , MethodChannel

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_webview_1/web_view.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter PlatformView API',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter PlatformView Example'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: Container(
+        constraints: const BoxConstraints.expand(),
+        child: const WebView(),
+      ),
+    );
+  }
+}
---
+++ lib/web_view.dart
@@ -0,0 +1,53 @@
+import 'dart:async';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class WebView extends StatefulWidget {
+  const WebView({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _WebViewState();
+}
+
+class _WebViewState extends State<WebView> {
+  static const platform = MethodChannel('samples.flutter.dev/battery');
+  final Map<String, dynamic> creationParams = <String, dynamic>{};
+  @override
+  Widget build(BuildContext context) {
+    const String viewType = '<platform-view-type>';
+
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      return AndroidView(
+        viewType: viewType,
+        layoutDirection: TextDirection.ltr,
+        creationParams: creationParams,
+        creationParamsCodec: const StandardMessageCodec(),
+        onPlatformViewCreated: _onPlatformViewCreated,
+      );
+    }
+    return Text('$defaultTargetPlatform is not supported!');
+  }
+
+  void _onPlatformViewCreated(int id) {
+    _getBatteryLevel();
+  }
+
+  String _batteryLevel = 'Unknown battery level.';
+
+  Future<void> _getBatteryLevel() async {
+    String batteryLevel;
+    try {
+      final String result = await platform.invokeMethod('setUrl');
+      batteryLevel = 'Battery level at $result % .';
+    } on PlatformException catch (e) {
+      batteryLevel = "Failed to get battery level: '${e.message}'.";
+    }
+
+    setState(() {
+      _batteryLevel = batteryLevel;
+    });
+  }
+}
